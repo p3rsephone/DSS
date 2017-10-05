9abb29b6027e40e07b71e50f8b3b93295dd97729
ZePedroResende <j.p.resende68@gmail.com>
2017-10-05 14:40:45 +0100

    Revert "Zpr swing (#5)"
    This reverts commit 631d632948336b79ce112cb31932c31f21d15001.

--- mini-trabalho/src/CesiumModel/Cesium.java
+++ mini-trabalho/src/CesiumModel/Cesium.java
@@ -1,88 +1,41 @@
-package CesiumModel;
-
+package CesiumController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import CesiumModel.Aluno;
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Observable;
 
-public class Cesium extends Observable implements Serializable{
+public class Cesium {
     private HashMap<Integer, Aluno> cesium;
-    private String date; 
-
-    public Cesium(HashMap<Integer, Aluno> cesium) {
-	this.cesium = cesium;
-    }
 
     public Cesium() {
         cesium = new HashMap<>();
     }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-
-    
-
     public Map<Integer, Aluno> getAlunos() {
-        HashMap<Integer, Aluno> copy = new HashMap<>();
+        HashMap<Integer, Aluno> copy = new HashMap<Integer, Aluno>();
         for (Map.Entry<Integer, Aluno> entry : cesium.entrySet())
         {
-            copy.put(entry.getKey(), entry.getValue().clone());
-        } 
+            copy.put(entry.getKey(), ((Aluno) entry).clone());
+        }
         return copy;
     }
 
-    public void updateQuotas(){
 
-        for (Map.Entry<Integer, Aluno> entry : cesium.entrySet())
-        {
-
-	 String DATE_FORMAT_NOW = "dd-MM-yyyy";
-	 Date date = new Date();
-	 SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-	 String stringDate = sdf.format(date );
-
-	    entry.getValue().getValores().add(stringDate);
-            cesium.put(entry.getKey(), entry.getValue());
-	}
-	   
-
-        } 
     public void pagarQuota(Integer numero, Double valor){
         Aluno socio = cesium.get(numero);
-	List a = socio.getValores();
-        if(valor >= 5)
- 		a.remove(0);
+        if(valor >= 7.5)
+            socio.setQuotas(true);
+
     }
 
     public Aluno getAluno(int num)
     {
         return cesium.get(num);
     }
-    
-    public void removeAluno(int num){
-    	cesium.remove(num);
-	this.setChanged();
-	this.notifyObservers();
-    }
 
     public void addAluno(Aluno a){
         cesium.put( a.getNumero() ,a.clone());
-	this.setChanged();
-	this.notifyObservers();
     }
 
 }
