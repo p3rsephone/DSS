67ea33f959d26142ed4b15de8f5548698f6f8481
Resende <j.p.resende68@gmail.com>
2017-12-07 23:43:33 +0000

    minor details that were missing (#14)
    * minor details that were missing
    
    * fix jdk
    
    * fix default
    
    * fix default
    
    * fix default
    
    * fix default
    
    * gitingore fix
    
    * removido .class

--- Project/src/business/users/TeacherBoss.java
+++ Project/src/business/users/TeacherBoss.java
@@ -6,8 +6,8 @@ public class TeacherBoss extends Teacher{
     private HashSet<String> courses;
 
 
-    public TeacherBoss(String name, String email, String password) {
-        super(name, email, password);
+    public TeacherBoss(String name, Integer number, String email, String password) {
+        super(name, number, email, password);
         this.courses = new HashSet<>();
     }
 }
