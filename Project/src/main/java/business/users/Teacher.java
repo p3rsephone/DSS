e3df358aa9a28232e381330479f1cfbe27bd995d
jhugobb <hugo.b.brandao@gmail.com>
2017-12-21 21:53:36 +0000

    Add teacher to the new method of login

--- Project/src/main/java/business/users/Teacher.java
+++ Project/src/main/java/business/users/Teacher.java
@@ -6,6 +6,6 @@ public class Teacher extends User {
     private Set<String> shifts;
 
     public Teacher(String name, Integer number, String email, String password) {
-        super(name, number, username, email, password);
+        super(name, number, email, password);
     }
 }
