98bb63ac0d45713138c98bbd1b5216e734b932b6
resende <resende@red.red.lan>
2017-12-23 16:12:31 +0000

    added students ui with request dialog

---
+++ Project/src/main/java/business/users/DC.java
@@ -0,0 +1,8 @@
+package business.users;
+
+public class DC extends User {
+
+    public DC(String name, Integer number, String email, String password) {
+        super(name, number, email, password);
+    }
+}
