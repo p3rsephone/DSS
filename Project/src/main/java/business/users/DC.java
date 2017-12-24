2ee63712871532d49767feb8315342f80cbffe8f
resende <resende@red.red.lan>
2017-12-24 16:50:14 +0000

    dc

--- Project/src/main/java/business/users/DC.java
+++ Project/src/main/java/business/users/DC.java
@@ -2,7 +2,7 @@ package business.users;
 
 public class DC extends User {
 
-    public DC(String name, Integer number, String email, String password) {
-        super(name, number, email, password);
+    public DC() {
+        super("DC", 0, "dc@dc.pt", "dc");
     }
 }
