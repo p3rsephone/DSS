5191899853d8cf199d14528e6becd8ab4d82c975
resende <resende@red.red.lan>
2017-12-24 03:58:32 +0000

    things ... its late as fuck i dont remember stuffs

--- Project/src/main/java/parser/Parser.java
+++ Project/src/main/java/parser/Parser.java
@@ -1,5 +1,8 @@
 package parser;
+import business.Engine;
 import business.courses.Course;
+import business.exceptions.ShiftAlredyExistsException;
+import business.exceptions.UserAlredyExistsException;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
@@ -13,14 +16,15 @@ import java.util.Map;
 
 
 public class Parser {
-    private HashMap<String, Course> courses;
+    private Engine engine;
     private Gson gson ;
 
-    public Parser(HashMap<String,Course> courses) {
-        this.courses = courses;
+    public Parser(Engine engine) {
+        this.engine = engine;
         this.gson = new Gson();
     }
 
+    /*
     public void parseMscProfiles(String file) throws FileNotFoundException {
         JsonReader jsonReader = new JsonReader(new FileReader(file));
         JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
@@ -30,11 +34,87 @@ public class Parser {
         data.forEach((key,value)->
             value.forEach((k,v)-> {
                 String id = k.replace("UC", key);
-                courses.put(id,new Course(id, v.getNome(), null , 4));
+                engine.
             })
         );
     }
+    */
 
+    public void parseCourse(String file) throws FileNotFoundException {
+        JsonReader jsonReader = new JsonReader(new FileReader(file));
+        JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
+        Type type = new TypeToken<Map<String, parser.Course>>(){}.getType();
+        Map<String, parser.Course> data = gson.fromJson(jsonFile, type);
+
+        data.forEach((key,value)->
+            engine.addCourse(new Course(value.getCodigo(),value.getNome(),value.getTeacherReg(),value.getYear()))
+        );
+    }
+
+    public void parseShift(String file) throws FileNotFoundException {
+        JsonReader jsonReader = new JsonReader(new FileReader(file));
+        JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
+        Type type = new TypeToken<Map<String, Shift>>(){}.getType();
+        Map<String,Shift> data = gson.fromJson(jsonFile, type);
+
+        data.forEach((key,value)->{
+            Course c = engine.getCourse(value.getCourse());
+                    try {
+                        c.addShift(new business.courses.Shift(key,value.getCourse(),value.getLimit(),value.getTeacher(),value.getClasses(),value.getRoomcode(),value.getWeekday(),value.getPeriod()));
+                        engine.addCourse(c);
+                    } catch (ShiftAlredyExistsException e) {
+                        e.printStackTrace();
+                    }
+        }
+        );
+    }
+    public void parseRoom(String file) throws FileNotFoundException {
+        JsonReader jsonReader = new JsonReader(new FileReader(file));
+        JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
+        Type type = new TypeToken<Map<String, Room>>(){}.getType();
+        Map<String,Room> data = gson.fromJson(jsonFile, type);
+
+        data.forEach((key,value)->{
+            engine.addRoom(new business.courses.Room(key,value.getCapacidade()));
+        }
+        );
+    }
+    public void parseTeacher(String file) throws FileNotFoundException {
+        JsonReader jsonReader = new JsonReader(new FileReader(file));
+        JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
+        Type type = new TypeToken<Map<String,Teacher>>(){}.getType();
+        Map<String,Teacher> data = gson.fromJson(jsonFile, type);
+
+        data.forEach((key,value)->{
+            boolean boss = value.getBoss().equals("y");
+                    try {
+                        engine.addUser(new business.users.Teacher(value.getName(),value.getNumber(),value.getEmail(),value.getPassword(),boss));
+                    } catch (UserAlredyExistsException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        );
+    }
+
+    public void parseStudent(String file) throws FileNotFoundException {
+        JsonReader jsonReader = new JsonReader(new FileReader(file));
+        JsonObject jsonFile = gson.fromJson(jsonReader, JsonObject.class);
+        Type type = new TypeToken<Map<Integer, Student>>(){}.getType();
+        Map<Integer,Student> data = gson.fromJson(jsonFile, type);
+
+        data.forEach((key,value)-> {
+                    try {
+                        engine.addUser(new business.users.Student(value.getName(),value.getEmail(),value.getPassword(),key,Boolean.valueOf(value.getStatute())));
+                    } catch (UserAlredyExistsException e) {
+                        e.printStackTrace();
+                    }
+                    for(String course : value.getCourses()){
+                        engine.enrollStudent(course,key);
+                    }
+                }
+        );
+    }
     /*
     public static void main(String[] args) {
         HashMap<String, Course> courses = new HashMap<>();
---
+++ Project/src/main/java/parser/Student.java
@@ -0,0 +1,59 @@
+package parser;
+
+import java.util.List;
+
+public class Student {
+    private String name;
+    private String email;
+    private String password;
+    private String statute;
+    private List<String> courses;
+
+    public Student(String name, String email, String password, String statute, List<String> courses) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.statute = statute;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatute() {
+        return statute;
+    }
+
+    public void setStatute(String statute) {
+        this.statute = statute;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<String> courses) {
+        this.courses = courses;
+    }
+}
